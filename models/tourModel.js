955e63020670268765b9e1935f54db784d71260f
Kevin Rangel Moreira <rangel.moreira@outlook.com>
2023-06-29 19:05:12 -0300

    DOCUMENT MIDDLWARE and Slugify

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 //Import Mongoose
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const tourSchema = new mongoose.Schema(
   {
@@ -9,6 +10,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, "A tour must have a duration"],
@@ -60,6 +62,22 @@ const tourSchema = new mongoose.Schema(
     toObject: { virtuals: true },
   }
 );
+
+//DOCUMENT MIDDLWARE: runs before .save() and .create()
+tourSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.pre("save", function (next) {
+//   console.log("Will save document...");
+//   next();
+// });
+
+// tourSchema.post("save", function (doc, next) {
+//   console.log(doc);
+//   next();
+// });
 
 tourSchema.virtual("durationWeeks").get(function () {
   return this.duration / 7;

78331cf6f1d53d0989bb7cae240b8045cf138527
Kevin Rangel Moreira <rangel.moreira@outlook.com>
2023-07-09 11:38:40 -0300

    Handling Unhandled Routes

--- app.js
+++ app.js
@@ -19,4 +19,11 @@ app.use(bodyParser.json());
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    statu: "fail",
+    message: `Can't find ${req.originalUrl} on this server`,
+  });
+});
+
 module.exports = app;

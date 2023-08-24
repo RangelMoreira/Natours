9aa4362eee7619860a113195295832592fdf0520
Kevin Rangel Moreira <rangel.moreira@outlook.com>
2023-08-23 21:11:34 -0300

    Updating the Current User: Data

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,6 +12,18 @@ const signToken = (id) => {
   });
 };
 
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user,
+    },
+  });
+};
+
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
@@ -22,19 +34,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     //passwordChangedAt: new Date(req.body.passwordChangedAt),
   });
 
-  //Store in token
-  const token = signToken(newUser._id);
-  // const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-  //   expiresIn: process.env.JWT_EXPIRES_IN,
-  // });
-
-  res.status(201).json({
-    status: 'success',
-    token,
-    data: {
-      user: newUser,
-    },
-  });
+  createSendToken(newUser, 201, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -180,10 +180,23 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   // 3) Update changedPasswordAt property for the user
 
   // 4) Log the user in, send JWT
-  const token = signToken(user._id);
+  createSendToken(user, 200, res);
+});
 
-  res.status(200).json({
-    status: 'sussess',
-    token,
-  });
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  //1) Get user from collection
+  const user = await User.findById(req.user.id).select('+password');
+
+  //2) Check if Posted current password is correct
+  if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+    return next(new AppError('Your current password is wrong'), 401);
+  }
+  //3) If so, update password
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
+  //User.findAndUpdate will not work as instead!
+
+  //4) Log user in, send JWT
+  createSendToken(user, 200, res);
 });
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,12 @@
-const catchAsync = require("./../utils/catchAsync");
-const User = require("../models/userModel");
+const catchAsync = require('./../utils/catchAsync');
+const User = require('../models/userModel');
 
 exports.getAllUsers = catchAsync(async (req, res) => {
   const users = await User.find();
 
   //SEND RESPONSE
   res.status(200).json({
-    status: "success",
+    status: 'success',
     results: users.length,
     data: {
       users,
@@ -16,25 +16,25 @@ exports.getAllUsers = catchAsync(async (req, res) => {
 
 exports.getUser = (req, res) => {
   res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined",
+    status: 'error',
+    message: 'This route is not yet defined',
   });
 };
 exports.createUser = (req, res) => {
   res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined",
+    status: 'error',
+    message: 'This route is not yet defined',
   });
 };
 exports.updateUser = (req, res) => {
   res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined",
+    status: 'error',
+    message: 'This route is not yet defined',
   });
 };
 exports.deleteUser = (req, res) => {
   res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined",
+    status: 'error',
+    message: 'This route is not yet defined',
   });
 };

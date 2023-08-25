401aefe0ed1996f25c957cce9e121861742409c3
Kevin Rangel Moreira <rangel.moreira@outlook.com>
2023-08-25 20:44:03 -0300

    returning object

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,15 @@ const catchAsync = require('./../utils/catchAsync');
 const User = require('../models/userModel');
 const AppError = require('./../utils/appError');
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  //Keys get the kewords(atribute name) of objects
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
 exports.getAllUsers = catchAsync(async (req, res) => {
   const users = await User.find();
 
@@ -25,18 +34,24 @@ exports.updateMe = catchAsync(async (req, res, next) => {
       )
     );
   }
-  //2) Update user document
+  //2) Filtered out unwante3d fields name that are not allowed to be updated
   //As it is not necessary to update the password, we will use findByIdUpdate
-  //The data passed called X, We are x because we not need updating all body fields
+  //The data passed called filteresBody, We are x because we not need updating all body fields
   //new: true -> returns a new object
   //runValidator is necessary because mongoose need to validate
 
-  /*const user = await User.findByIdUpdate(req.user.id, x, {
+  const filteresBody = filterObj(req.body, 'name', 'email');
+
+  //3) Update user Document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteresBody, {
     new: true,
     runValidator: true,
-  });*/
+  });
   res.status(200).json({
     status: 'success',
+    data: {
+      user: updatedUser,
+    },
   });
 });
 exports.getUser = (req, res) => {

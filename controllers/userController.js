bc2a59cbbfca332942072bd6a5b8f7a61e3c300d
Kevin Rangel Moreira <rangel.moreira@outlook.com>
2023-08-24 21:53:06 -0300

    Updating the Current User: Data part 1

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const catchAsync = require('./../utils/catchAsync');
 const User = require('../models/userModel');
+const AppError = require('./../utils/appError');
 
 exports.getAllUsers = catchAsync(async (req, res) => {
   const users = await User.find();
@@ -14,6 +15,30 @@ exports.getAllUsers = catchAsync(async (req, res) => {
   });
 });
 
+exports.updateMe = catchAsync(async (req, res, next) => {
+  //1) Create error if user POSTS password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword',
+        400
+      )
+    );
+  }
+  //2) Update user document
+  //As it is not necessary to update the password, we will use findByIdUpdate
+  //The data passed called X, We are x because we not need updating all body fields
+  //new: true -> returns a new object
+  //runValidator is necessary because mongoose need to validate
+
+  /*const user = await User.findByIdUpdate(req.user.id, x, {
+    new: true,
+    runValidator: true,
+  });*/
+  res.status(200).json({
+    status: 'success',
+  });
+});
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',

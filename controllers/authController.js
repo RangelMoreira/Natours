4e77b9c6a2fe8683e2b97b89d6b48485e52dc8cd
Kevin Rangel Moreira <rangel.moreira@outlook.com>
2023-08-30 20:14:41 -0300

    Sending JWT via Cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,8 +12,22 @@ const signToken = (id) => {
   });
 };
 
+const cookieOptions = {
+  expires: new Date(
+    Date.now() + parseInt(process.env.JWT_EXPIRES_IN, 10) * 24 * 60 * 60 * 1000
+  ),
+  httpOnly: true, //Prevents the cookie from being changed by the browser
+};
+
+if (process.env.NODE_ENV === 'production') cookieOptions.secure = true; //The cookie will only be sent when HTTPS
+
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+
+  res.cookie('jwt', token, cookieOptions);
+
+  //Remove the password from the output
+  user.password = undefined;
 
   res.status(statusCode).json({
     status: 'success',

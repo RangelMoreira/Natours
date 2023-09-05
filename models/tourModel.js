36380a0ec09a43f5c2649751d9b72d818b9eb75e
Kevin Rangel Moreira <rangel.moreira@outlook.com>
2023-09-05 20:26:04 -0300

    Modelling Tour Guides: Embedding.

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,7 @@
 //Import Mongoose
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./userModel');
 // const validator = require("validator");
 
 const tourSchema = new mongoose.Schema(
@@ -103,6 +104,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -113,6 +115,12 @@ const tourSchema = new mongoose.Schema(
 //DOCUMENT MIDDLWARE: runs before .save() and .create()
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
   next();
 });
 

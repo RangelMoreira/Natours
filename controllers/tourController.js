562a45fee9edaa40f0af8e1fb1d9e2860cc2fb75
Kevin Rangel Moreira <rangel.moreira@outlook.com>
2023-09-10 13:48:16 -0300

    Virtual Populate: Tours and Reviews

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -17,6 +17,7 @@ exports.getTour = catchAsync(async (req, res, next) => {
   // });
   //const tour = await Tour.findById(req.params.id);
   const tour = await Tour.findById(req.params.id).populate('reviews');
+
   if (!tour) {
     return next(new AppError('Tour not found with that id', 404));
   }

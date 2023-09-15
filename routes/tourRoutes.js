f004e6d3b508924c288ba75129ff0ca229252b82
Kevin Rangel Moreira <rangel.moreira@outlook.com>
2023-09-14 21:00:50 -0300

    Nested Routes with Express

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
-const reviewController = require('./../controllers/reviewController');
+const reviewRouter = require('./../routes/reviewRoutes');
 
 const router = express.Router();
+
+//POST /tour/234fad4/reviews
+//GET /tour/234fad4/reviews
+//GET  /tour/234fad4/reviews/94887fda
+
+// router
+//   .route('/:tourId/reviews')
+//   .post(
+//     authController.protect,
+//     authController.restrictTo('user'),
+//     reviewController.createReview
+//   );
+
+router.use('/:tourId/reviews', reviewRouter);
 
 // router.param('id', tourController.checkId);
 
@@ -34,18 +48,6 @@ router
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour
-  );
-
-//POST /tour/234fad4/reviews
-//GET /tour/234fad4/reviews
-//GET  /tour/234fad4/reviews/94887fda
-
-router
-  .route('/:tourId/reviews')
-  .post(
-    authController.protect,
-    authController.restrictTo('user'),
-    reviewController.createReview
   );
 
 module.exports = router;

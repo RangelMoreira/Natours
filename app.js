d59c90153f636c52e5b1100bf365f15e55542f1c
Kevin Rangel Moreira <rangel.moreira@outlook.com>
2023-10-21 12:59:34 -0300

    First Steps with Pug

--- app.js
+++ app.js
@@ -66,7 +66,10 @@ app.use(bodyParser.json());
 
 //3) ROUTES
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Kevin',
+  });
 });
 
 app.use('/api/v1/tours', tourRouter);
